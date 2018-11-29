9d82807e1ce71b4296621d9d03d5201ec4f28e0e
Martin Bee <martinb@nobrainer2.com>
2018-11-29 09:16:42 -0600

    feat: get simple search working

--- src/background.js
+++ src/background.js
@@ -1,9 +1,22 @@
+var baseSearchUrls = [
+  'https://shop.tcgplayer.com/productcatalog/product/show?newSearch=false&ProductType=All&IsProductNameExact=false&ProductName=',
+  'https://scryfall.com/search?q=',
+  'http://tappedout.net/search/?q='
+];
+
+function openUpSearchTabs(searchTerm) {
+  baseSearchUrls.forEach(function(baseUrl) {
+    chrome.tabs.create({ url: baseUrl + searchTerm });
+  });
+}
+
 var mtgSearchMenuProps = {
   title: "Search for '%s'?",
   visible: true,
   contexts: ['selection'],
   onclick(info) {
     console.log(info.selectionText);
+    openUpSearchTabs(info.selectionText);
   }
 };
 
